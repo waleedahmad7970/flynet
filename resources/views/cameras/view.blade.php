d487eae35483ad359892d883c5bfef2ae9018e37
Yasir Sherwani <yasir.sherwani@gmail.com>
2025-08-01 16:04:19 +0500

    hls video source

--- resources/views/cameras/view.blade.php
+++ resources/views/cameras/view.blade.php
@@ -191,7 +191,7 @@
 
     const video = document.getElementById('video');
  
-    const videoSrc = 'http://127.0.0.1:8888/{{ $camera->slug }}/index.m3u8';
+    const videoSrc = 'http://168.227.22.23:8888/{{ $camera->slug }}/index.m3u8';
 
 	const hls = new Hls({
 		maxBufferLength: 30,
