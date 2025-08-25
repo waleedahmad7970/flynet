09d2164533d0922c9d87a4e9c82cb1f207b943a0
Yasir Sherwani <yasir.sherwani@gmail.com>
2025-08-25 18:09:36 +0500

    url change 3

--- resources/views/cameras/view.blade.php
+++ resources/views/cameras/view.blade.php
@@ -253,8 +253,8 @@
 
     const video = document.getElementById('video');
 
-   // const HLS_URL = 'http://168.227.22.23:8888/{{ $camera->slug }}/index.m3u8';
-    const HLS_URL = 'http://127.0.0.1:8888/{{ $camera->slug }}/index.m3u8';
+    const HLS_URL = 'http://168.227.22.23:8888/{{ $camera->slug }}/index.m3u8';
+    // const videoSrc = 'http://127.0.0.1:8888/{{ $camera->slug }}/index.m3u8';
 
 	// if (Hls.isSupported()) {
 	// 	const hls = new Hls({
